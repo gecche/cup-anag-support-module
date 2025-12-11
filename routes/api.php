be7d63e3a6d91523ee59935c2c7c178a026be451
Giacomo Terreni <giacomo.terreni@gmail.com>
2025-12-11 08:58:54 +0100

    CupAnagSupport bugfix

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Unico\Http\Controllers\CupAnagSupportController;
+use Modules\CupAnagSupport\Http\Controllers\CupAnagSupportController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::apiResource('cup-anag-supports', CupAnagSupportController::class)->names('cup-anag-support');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Unico\Http\Controllers\CupAnagSupportController;
+use Modules\CupAnagSupport\Http\Controllers\CupAnagSupportController;
 
 
 Route::middleware(['auth', 'verified'])->group(function () {
